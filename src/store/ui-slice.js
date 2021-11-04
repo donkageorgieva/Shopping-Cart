a21efc4182676daed44b9a8c9e451a2fe64aa7f0
donkageorgieva <DoniGeorgieva16@gmail.com>
2021-11-04 18:10:35 +0200

    notification redux slice

--- src/store/ui-slice.js
+++ src/store/ui-slice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
-  name: "ui",
+  name: "notification",
   initialState: { notification: null },
   reducers: {
     toggleNotification(state, action) {
